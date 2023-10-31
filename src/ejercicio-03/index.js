16195807529cd8bead1d02a3a774331d85d654af
Maggie <guachachi02@yahoo.es>
2023-10-31 22:33:26 +0100

    Añadir ejercicio-03

--- src/ejercicio-03/index.js
+++ src/ejercicio-03/index.js
@@ -1,21 +1,20 @@
 const numero = 99
 
-const tipoNumero = numero > 0 ? "positivo" : "negativo o cero"
-console.log("El número es ${tipoNumero}")
-// Resultado: 
+const tipoNumero = numero > 0 ? `positivo` : `negativo o cero`
+console.log(`El número es ${tipoNumero}`)
 
 const cadena = "Hola BlockMaker"
 
 const longitudCadena = cadena.length > 5 ? "larga" : "corta"
-console.log("La cadena es ${longitudCadena}")
-// Resultado:
+console.log(`La cadena es ${longitudCadena}`)
 
-funtion esPar(numero) {
-    const numeroPar = numero ? "true" : "false"
-console.log("El número es par" ${numeroPar})
+function esPar(numero) {
+    return numero % 2 === 0
 }
-// Resultado:
 
-funtion esPar(numero)
-console.log(numero)
-// Resultado:
+const numeroPar = 4
+if (esPar(numeroPar)) {
+    console.log(`${numeroPar} es par`)
+} else {
+    console.log(`${numeroPar} es impar`)
+}
